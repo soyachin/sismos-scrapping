b64162d2dedc753e8f4294fabc114927524e6b0b
Kris <miranda.salazar@utec.edu.pe>
2024-11-10 22:30:57 -0500

    sismos fix

--- sismos.py
+++ sismos.py
@@ -35,14 +35,15 @@ def lambda_handler(event, context):
             'body': 'No se encontró la tabla en la página web'
         }
 
-    # Extraer los encabezados de la tabla
     headers = [header.text for header in table.find_all('th')]
 
     # Extraer las filas de la tabla
     rows = []
     for row in table.find_all('tr')[1:]:  # Omitir el encabezado
         cells = row.find_all('td')
-        rows.append({headers[i+1]: cell.text for i, cell in enumerate(cells)})
+        if len(cells) != len(headers):
+            continue  # Skip rows that don't match the header length
+        rows.append({headers[i]: cell.text for i, cell in enumerate(cells)})
 
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
@@ -52,22 +53,15 @@ def lambda_handler(event, context):
     scan = table.scan()
     with table.batch_writer() as batch:
         for each in scan['Items']:
-            batch.delete_item(
-                Key={
-                    'id': each['id']
-                }
-            )
+            batch.delete_item(Key={'id': each['id']})
 
-    # Insertar los nuevos datos
-    i = 1
-    for row in rows:
-        row['#'] = i
-        row['id'] = str(uuid.uuid4())  # Generar un ID único para cada entrada
-        table.put_item(Item=row)
-        i = i + 1
+    # Agregar los nuevos elementos a la tabla
+    with table.batch_writer() as batch:
+        for row in rows:
+            row['id'] = str(uuid.uuid4())
+            batch.put_item(Item=row)
 
-    # Retornar el resultado como JSON
     return {
         'statusCode': 200,
-        'body': rows
+        'body': 'Datos actualizados correctamente'
     }
